afdb7982bc1d622ef129a2b7fee44fe59d7b14bb
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-02-22 18:00:50 -0300

    All done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,41 @@ import { Canvas } from "@react-three/fiber";
 import { motion } from "framer-motion-3d";
 
 
-export function Laptop(props) {
+export function Laptop({isOpen, ...props}) {
   const { nodes, materials } = useGLTF('/laptop.glb')
+  const variants = {
+    open: {
+      rotateX: (0 * Math.PI) / 180,
+      x: -0.03,
+      y: 111.69,
+      z: 2.84
+    },
+    closed: {
+      rotateX: (105 * Math.PI) / 180,
+      x: 0,
+      y: 75,
+      z: 39
+    }
+  }
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={0.03}>
-        <group position={[-0.03, 111.69, 2.84]} scale={3.37}>
+        <motion.group
+          position={[-0.03, 111.69, 2.84]}
+          scale={3.37}
+          initial={"closed"}
+          animate={isOpen? "open" : "closed"}
+          transition={{
+            delay: 1,
+            duration: 1
+          }}
+          variants={variants}
+        >
           <mesh geometry={nodes.Screen.geometry} material={materials.Namibia} position={[0.01, -33.13, -0.84]} scale={0.3} />
           <mesh geometry={nodes.Black_Inset.geometry} material={materials['Display Rim 1']} position={[0.01, -33.13, -0.84]} scale={0.3} />
           <mesh geometry={nodes.Rubber.geometry} material={materials['Display Rubber 1']} position={[0.01, -33.13, -0.84]} scale={0.3} />
           <mesh geometry={nodes.Screen_Back.geometry} material={materials['Body 2']} position={[0.01, -33.13, -0.84]} scale={0.3} />
-        </group>
+        </motion.group>
         <mesh geometry={nodes.Keyboard.geometry} material={materials['Keyboard 2']} />
         <mesh geometry={nodes.Touch_Panel.geometry} material={materials['Touch Bar 1']} />
         <mesh geometry={nodes.Ports.geometry} material={materials['Ports 1']} />
@@ -35,7 +59,7 @@ function App() {
           <ambientLight intensity={0.8} />
           <directionalLight color="red" position={[0, 0, 5]} />
           <pointLight position={[10, 10, 50]} />
-          <Laptop />
+          <Laptop isOpen={isOpen}/>
           <PerspectiveCamera makeDefault position={[0, 0, 20]} />
           <OrbitControls />
         </Canvas>
